314cd8f7034ef420dfc06699400910b7b2cdbb4e
Argenis Ochoa Gonzalez <argenis8agonzalez@gmail.com>
2021-02-17 00:12:15 -0600

    modified:   megademy-dl:v2/entrypoint.sh

--- megademy-dl:v2/entrypoint.sh
+++ megademy-dl:v2/entrypoint.sh
@@ -9,7 +9,14 @@ python /usr/src/udemy-dl/udemy-dl.py \
 $ARGS
 NAME=`ls /data`
 rm /data/.udemy-dl.conf
+rm /data/$NAME/udemy-dl.log
 tar -cvj /data |split -b ${SIZEVOLUME} -d - $NAME$FECHA.tbz.
 megamkdir -u ${MAILMEGA} -p ${PASSWORDMEGA} /Root/$FECHA/
+cat <<EOF > join.sh
+#!/bin/sh
+cat $NAME$FECHA.tbz.* > $NAME$FECHA.tbz2
+EOF
+chmod +x join.sh
 megaput -u ${MAILMEGA} -p ${PASSWORDMEGA} --path=/Root/$FECHA/ $NAME$FECHA.tbz.*
+megaput -u ${MAILMEGA} -p ${PASSWORDMEGA} --path=/Root/$FECHA/ join.sh
 rm /data/ -Rv
