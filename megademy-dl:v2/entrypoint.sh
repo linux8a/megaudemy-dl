779b681e283b41f1970c28e6c392365e3dc6619f
Argenis Ochoa Gonzalez <argenis8agonzalez@gmail.com>
2021-02-17 21:39:31 -0600

    Creando carpeta en mega con el nombre del curso:   megademy-dl:v2/entrypoint.sh

--- megademy-dl:v2/entrypoint.sh
+++ megademy-dl:v2/entrypoint.sh
@@ -11,12 +11,12 @@ NAME=`ls /data`
 rm /data/.udemy-dl.conf
 rm /data/$NAME/udemy-dl.log
 tar -cvj /data |split -b ${SIZEVOLUME} -d - $NAME$FECHA.tbz.
-megamkdir -u ${MAILMEGA} -p ${PASSWORDMEGA} /Root/$FECHA/
+megamkdir -u ${MAILMEGA} -p ${PASSWORDMEGA} /Root/$NAME/
 cat <<EOF > join.sh
 #!/bin/sh
 cat $NAME$FECHA.tbz.* > $NAME$FECHA.tbz2
 EOF
 chmod +x join.sh
-megaput -u ${MAILMEGA} -p ${PASSWORDMEGA} --path=/Root/$FECHA/ $NAME$FECHA.tbz.*
-megaput -u ${MAILMEGA} -p ${PASSWORDMEGA} --path=/Root/$FECHA/ join.sh
+megaput -u ${MAILMEGA} -p ${PASSWORDMEGA} --path=/Root/$NAME/ $NAME$FECHA.tbz.*
+megaput -u ${MAILMEGA} -p ${PASSWORDMEGA} --path=/Root/$NAME/ join.sh
 rm /data/ -Rv
